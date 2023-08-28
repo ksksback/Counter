5c12fd6ef62200567f4ad63f8a057800347a3ed7
Ксюша <madam.marsianovna@yandex.ru>
2023-08-28 08:10:23 +0300

    Создание приложение счетчика (базовая задача)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counterNumber: Int = 0
 
+   
+    @IBOutlet weak var buttonCounter: UIButton!
+    @IBOutlet weak var counter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter.text = "0"
     }
 
-
+    @IBAction func buttonTap(_ sender: Any) {
+        self.counterNumber += 1
+        self.counter.text = String(self.counterNumber)
+        //преобразовала int в строку для отображения
+    }
+        
 }
+    
+
 
